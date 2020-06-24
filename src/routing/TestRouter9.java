bfa66a2982a5a2b40d7ccada0aeb118e793cb074
Fiona <fionagoga21@gmail.com>
2020-06-24 22:32:21 +0200

    update contact duration and add class diagram

--- src/routing/TestRouter9.java
+++ src/routing/TestRouter9.java
@@ -19,7 +19,7 @@ public class TestRouter9 extends ActiveRouter
 {
     List<ContactMetadata> contactMetrics;
     private Map<DTNHost, StorageMetadata> storageMetrics;
-    private Map<DTNHost, UniqueContactMetadata> contactMetadata;
+    private Map<String, UniqueContactMetadata> contactMetadata;
     
    
     public TestRouter9(Settings s) {
@@ -34,47 +34,67 @@ public class TestRouter9 extends ActiveRouter
     private void initBufferSizes() {
         this.contactMetrics = new ArrayList<ContactMetadata>();
         this.storageMetrics = new HashMap<DTNHost, StorageMetadata>();
-        this.contactMetadata = new HashMap<DTNHost, UniqueContactMetadata>();
+        this.contactMetadata = new HashMap<String, UniqueContactMetadata>();
     }
    
     @Override
     public void changedConnection(Connection con) {
+        DTNHost otherHost = con.getOtherNode(getHost());;
         if (con.isUp()) {
        
-            DTNHost otherHost = con.getOtherNode(getHost());
             updateMetricsFor(otherHost);
             updateTransitiveMetrics((TestRouter9)otherHost.getRouter());
+        }else {
+          //System.out.println(SimClock.getTime());
+            updateContactDurationFor(otherHost);
+            updateUniqueContactMetricsFor(otherHost);    
         }
+        
     }
    
     private void updateMetricsFor(DTNHost host) {
-        int counter = 0;
-        double sum = 0.0;
-        double averageContactDuration = 0.0;
+
         StorageMetadata sm = new StorageMetadata();
         ContactMetadata cm = new ContactMetadata();
         sm.update(host);
         cm.update(host);
         contactMetrics.add(cm);
-        storageMetrics.put(host, sm);   
+        storageMetrics.put(host, sm);        
         
-        for(ContactMetadata contact : contactMetrics) {
-            if(contact.getHostName()==host.toString()) {
-                counter++;
-                sum+= contact.getContactDuration();
-            }
-            if(counter!=0) {
-                averageContactDuration= sum/counter;
-            }
-            UniqueContactMetadata uniqueContact = new UniqueContactMetadata(averageContactDuration, counter);
-            contactMetadata.put(host, uniqueContact);  
-        }
+    }
+    
+      private void updateContactDurationFor(DTNHost host) {
+          for(ContactMetadata contact : contactMetrics) {
+              if(contact.getHostName()==host.toString()) {
+                 double duration =  SimClock.getTime() - contact.getStartEncounterTime();
+                 contact.setContactDuration(duration);
+              }
+          }
+        
+    }
+    
+      private void updateUniqueContactMetricsFor(DTNHost host) {
+          int counter = 0;
+          double sum = 0.0;
+          double averageContactDuration = 0.0;
+          for(ContactMetadata contact : contactMetrics) {
+              if(contact.getHostName()==host.toString()) {
+                  counter++;
+                  sum+= contact.getContactDuration();
+              }
+              if(counter!=0) {
+                  averageContactDuration= sum/counter;
+              }
+              UniqueContactMetadata uniqueContact = new UniqueContactMetadata(averageContactDuration, counter);
+              contactMetadata.put(getHost().toString() + " " + "meets" + " " + host.toString(), uniqueContact);  
+          }
+        
     }
     
     private void updateTransitiveMetrics(TestRouter9 router) {
         Map<DTNHost, StorageMetadata> othersStorageMetadata =
                 router.getStorageMetrics();
-        Map<DTNHost, UniqueContactMetadata> othersContactMetadata =
+        Map<String, UniqueContactMetadata> othersContactMetadata =
                 router.getContactMetrics();
 
         for (Map.Entry<DTNHost, StorageMetadata> e : othersStorageMetadata.entrySet()) {
@@ -87,19 +107,22 @@ public class TestRouter9 extends ActiveRouter
 
         }
         
-        for (Map.Entry<DTNHost, UniqueContactMetadata> e : othersContactMetadata.entrySet()) {
-            DTNHost h = e.getKey();
+        for (Map.Entry<String, UniqueContactMetadata> e : othersContactMetadata.entrySet()) {
+            String s = e.getKey();
             UniqueContactMetadata cm = e.getValue();
-            if (h == getHost()) {
+            if (s.contains(getHost().toString())) {
+                System.out.println("fionaaaa"); //check later
                 continue; // don't add yourself
             }
-            contactMetadata.put(h,cm);
+                contactMetadata.put(s,cm);
+            
+           
 
         }
     }
     
    
-    private Map<DTNHost, UniqueContactMetadata> getContactMetrics() {
+    private Map<String, UniqueContactMetadata> getContactMetrics() {
         return this.contactMetadata;
     }
     
@@ -136,6 +159,7 @@ public class TestRouter9 extends ActiveRouter
                 continue; // do not send metadata message again
             }
         Message metadata = new Message(getHost(),other,"metadata", this.getContactMetrics().size() + this.getStorageMetrics().size());
+        //System.out.println(this.getContactMetrics().size() + this.getStorageMetrics().size());
         getHost().createNewMessage(metadata);
         startTransfer(metadata,con);
         }
@@ -170,15 +194,20 @@ public class TestRouter9 extends ActiveRouter
         if (messages.size() == 0) {
             return null;
         }
-
+        //System.out.println(messages.size());
         return tryMessagesForConnected(messages);   // try to send messages
     }
     
 
-    private String showInfoHost(DTNHost h) {
-String info = h.toString() + ": " + this.contactMetadata.get(h) + " " + this.contactMetadata.get(h);
+    private String showStorageInfoHost(DTNHost h) {
+String info = h.toString() + ": " + this.storageMetrics.get(h) + " " ;
 return info;
     }
+    
+    private String showContactInfo(String s) {
+        String info = s + ": " + this.contactMetadata.get(s);
+        return info;
+            }
 
     @Override
     public TestRouter9 replicate() {
@@ -192,7 +221,12 @@ return info;
 RoutingInfo ri = new RoutingInfo("Metadata Metrics");
 System.out.println();
 for (DTNHost key: getStorageMetrics().keySet()){
-ri.addMoreInfo(new RoutingInfo(String.format("%s ", showInfoHost(key))));
+ri.addMoreInfo(new RoutingInfo(String.format("%s ", showStorageInfoHost(key))));
+}
+System.out.println();
+System.out.println();
+for (String key: getContactMetrics().keySet()){
+ri.addMoreInfo(new RoutingInfo(String.format("%s ", showContactInfo(key))));
 }
 top.addMoreInfo(ri);
 
